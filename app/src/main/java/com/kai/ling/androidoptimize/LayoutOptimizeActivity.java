fe151b2aeea30d5b9ab9bf4eb162e69d30941ef6
kai.wang <377538289@qq.com>
2018-06-02 14:11:04 +0800

    布局merge

--- app/src/main/java/com/kai/ling/androidoptimize/LayoutOptimizeActivity.java
+++ app/src/main/java/com/kai/ling/androidoptimize/LayoutOptimizeActivity.java
@@ -1,13 +1,27 @@
 package com.kai.ling.androidoptimize;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewStub;
 
-public class LayoutOptimizeActivity extends AppCompatActivity {
+public class LayoutOptimizeActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout_optimize);
+        findViewById(R.id.tv_view_stub).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_view_stub:
+                ViewStub viewStubCompat = findViewById(R.id.view_stub);
+                viewStubCompat.inflate();
+                break;
+        }
     }
 }
